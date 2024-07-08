969d63a4d1d4107c057da1b69aa8c212056a73f8
Mario Di Miceli <mario.dimiceli@orange.com>
2024-07-08 23:15:37 +0200

    fix updated routes + bump dependencies

--- fetchers/metricsfetcher.go
+++ fetchers/metricsfetcher.go
@@ -98,7 +98,7 @@ func (f MetricsFetcher) Metrics(appIdOrPathOrName, metricPathDefault string, onl
 						wg.Done()
 						continue
 					}
-					log.Warnf("Cannot get metric for instance %s for instance id %s", j.Address, j.Tags.InstanceID)
+					log.Debugf("Cannot get metric for instance %s for instance id %s (%s/%s/%s)", j.Address, j.Tags.InstanceID, j.Tags.OrganizationName, j.Tags.SpaceName, j.Tags.AppName)
 					newMetrics = f.scrapeError(j, err)
 					metrics.MetricFetchFailedTotal.With(metrics.RouteToLabel(j)).Inc()
 				} else {
